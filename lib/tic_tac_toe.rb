57e4d959eb4a24735f0c78b5cda9abe0ebe44e4b
Marc Mills <mdmills1990@gmail.com>
2018-08-15 17:13:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -114,9 +114,9 @@ def play(board)
    turn(board)
   end
     if winner(board) == "X"
-      puts "Congratulations X!"
+      puts "Congratulations Asia!"
     elsif winner(board) == "O"
-      puts "Congratulations O!"
+      puts "Congratulations Marc!"
     elsif draw?(board) == true
        puts "Cat's Game!"
   end
